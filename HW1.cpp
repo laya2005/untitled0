0fd217878d790448ef02593f4be25113304edd4a
laya2005 <layalily2005@gmail.com>
2025-03-06 13:36:22 +0330

    Create HW1.cpp

---
+++ HW1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main() {
+    int a,b,c;
+    cin>>a>>b>>c;
+    if(a+b>c && a+c>b && b+c>a && abs(a-b)<c && abs(a-c)<b && abs(b-c)<a) {
+        cout<<"True";
+    }
+    else {
+        cout<<"False";
+    }
+    return 0;
+}
